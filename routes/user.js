e5ac7ab29f7b66dc72038022031cd5492b438a7e
tnploy <63070078@kmitl.ac.th>
2024-04-24 14:53:07 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -19,12 +19,14 @@ router.post("/signin", async (req, res, next) => {
   }
   const email = req.body.email;
   const password = req.body.password;
-  const conn = await pool.getConnection();
-  await conn.beginTransaction();
+
+  await pool.beginTransaction();
   try {
     // Check if username is correct
-    const [users] = await conn.query("SELECT * FROM users WHERE email=?", [email]);
+    const [users] = await pool.query("SELECT * FROM users WHERE email=?", [email]);
     const user = users[0];
+
+
     if (!user) {
       throw new Error("Incorrect username or password");
     }
@@ -33,12 +35,12 @@ router.post("/signin", async (req, res, next) => {
       throw new Error("Incorrect username or password");
     }
     // Check if token already existed
-    const [tokens] = await conn.query("SELECT * FROM access_tokens WHERE user_id=?", [user.user_id]);
-    const token = tokens[0].token;
+    const [tokens] = await pool.query("SELECT * FROM access_tokens WHERE user_id=?", [user.user_id]);
+    let token = tokens[0]?.token;
     if (!token) {
       // Generate and save token into database
       token = generateToken();
-      await conn.query("INSERT INTO access_tokens(user_id, token) VALUES (?, ?)", [user.user_id, token]);
+      await pool.query("INSERT INTO access_tokens(user_id, token) VALUES (?, ?)", [user.user_id, token]);
         if (user.role === 'applicant') {
           await pool.query('INSERT INTO students (user_id, email) VALUES (?, ?)',[user.user_id, email]);
         }else if (user.role === 'recruiter') {
@@ -48,13 +50,13 @@ router.post("/signin", async (req, res, next) => {
         await pool.query('INSERT INTO admin (user_id, email) VALUES (?, ?)', [user.user_id, email]);
     }
     }
-    conn.commit();
+    pool.commit();
     res.status(200).json({ token: token });
   } catch (error) {
-    conn.rollback();
+    pool.rollback();
     res.status(400).json(error.toString());
   } finally {
-    conn.release();
+    pool.release();
   }
 });
 
